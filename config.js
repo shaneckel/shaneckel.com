66f50c5f0bd27c3d6f96c68d7e6e5cf49977129a
Shane Eckel <shaneckel@gmail.com>
2017-03-28 19:55:18 -0400

    Uploading my personal site to github

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  ip : process.env.OPENSHIFT_NODEJS_IP || "localhost",
-  port : process.env.OPENSHIFT_NODEJS_PORT || 4000,
-  appname : process.env.OPENSHIFT_APP_NAME || "shane"
-}
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-#!/bin/env node
+var express = require('express'),
+    path = require('path'),
+    app = express();
 
-var config          = require('./config'),
-    express         = require('express'),
-    path            = require('path'),
-    app             = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
-app.configure(function () {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.static(path.join(__dirname + '/public')));
-  app.use(app.router);
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(app.router);
+
+app.get('*', function(req, res) {
+  res.render('index');
 });
 
-require('./routes.js')(app);
-
-app.listen( config.port, config.ip);
+app.listen(3000);
