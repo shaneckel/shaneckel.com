23c0fa75e0a8dfcebc6fdc561e91d96006332bcb
Shane Eckel <shaneckel@gmail.com>
2018-03-14 03:43:45 -0400

    git rid dat busted ass loader

--- components/Loading.vue
+++ components/Loading.vue
@@ -26,6 +26,7 @@ export default {
 </script>
 
 <style scoped>
+
 .loading-page {
   position: fixed;
   pointer-events: none;
@@ -34,15 +35,39 @@ export default {
   top: 0;
   background: rgba(255, 255, 255, 0);
   text-align: center;
-  font-size: 30px;
+  /* font-size: 30px; */
   font-family: sans-serif;
 }
 .loader{
   opacity: 0;
   position: fixed;
-  width: 2rem;
-  height: 2rem;
+  width: 2em;
+  height: 2em;
   background: white;
   left: 0;
+  /* margin: auto; */
+  -webkit-animation: sk-rotateplane 1.2s infinite ease-in-out;
+  animation: sk-rotateplane 1.2s infinite ease-in-out;
 }
+
+@-webkit-keyframes sk-rotateplane {
+  0% { -webkit-transform: perspective(120px) }
+  50% { -webkit-transform: perspective(120px) rotateY(180deg) }
+  100% { -webkit-transform: perspective(120px) rotateY(180deg)  rotateX(180deg) }
+}
+
+@keyframes sk-rotateplane {
+  0% {
+    transform: perspective(120px) rotateX(0deg) rotateY(0deg);
+    -webkit-transform: perspective(120px) rotateX(0deg) rotateY(0deg)
+  } 50% {
+    transform: perspective(120px) rotateX(-180.1deg) rotateY(0deg);
+    -webkit-transform: perspective(120px) rotateX(-180.1deg) rotateY(0deg)
+  } 100% {
+    transform: perspective(120px) rotateX(-180deg) rotateY(-179.9deg);
+    -webkit-transform: perspective(120px) rotateX(-180deg) rotateY(-179.9deg);
+  }
+}
+
+
 </style>
